d1a56f8aebb5d5b371789995bd4c3b33a223e2ff
Augusto Barbosa <augustobarbosa1988@gmail.com>
2024-03-01 09:37:26 -0300

    feat: login

---
+++ auth.config.ts
@@ -0,0 +1,31 @@
+import type { NextAuthConfig } from 'next-auth';
+ 
+export const authConfig = {
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl));
+      }
+      return true;
+    },
+  },
+  providers: [], // Add providers with an empty array for now
+} satisfies NextAuthConfig;
+
+
+
+/* const isLoggedIn = !!auth?.user;
+
+Então, isLoggedIn será true se auth?.user existir e não for null ou undefined,
+ caso contrário, será false. Este é um método conciso e seguro de garantir
+  que você tenha um valor booleano em uma determinada situação.
+
+*/
